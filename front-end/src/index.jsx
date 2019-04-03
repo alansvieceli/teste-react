6072c6a941c931be476bc5213969a489d52fa7ec
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-04-03 11:31:13 -0300

    adição de css

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,6 +1,12 @@
-import	React	from	'react'
-import	ReactDOM	from	'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './img/favicon.ico';
+
+import './css/index.css';
+import './css/pure-min.css';
+
 ReactDOM.render(
-			<h1>Bem-vindo	ao	React!</h1>,
-			document.querySelector("#main")
+  <h1>Bem-vindo ao React!</h1>,
+  document.querySelector("#main")
 )
