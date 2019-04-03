7e00ca41260c525dac9206958a63931e2c41a5f7
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-04-03 11:37:14 -0300

    adicionando extract-text-webpack-plugin e uglifyjs-webpack-plugin

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'src/index.jsx'),
@@ -15,7 +17,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.join(__dirname, 'src/index.html')
-    })
+    }),
+    new ExtractTextPlugin('style.css'),
+    new UglifyJSPlugin()
   ],
   module: {
     rules: [
@@ -38,11 +42,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" }
-        ]
-      }      
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+          })
+        }     
     ]
   },
   devServer: {
