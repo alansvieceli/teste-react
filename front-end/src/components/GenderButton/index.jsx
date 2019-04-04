ba07275d619c1d9fefc3fc64442f87a59e9c1f05
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-04-04 14:24:09 -0300

    Correções e ajustes

--- front-end/src/components/GenderButton/index.jsx
+++ front-end/src/components/GenderButton/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import './css/index.css'
+import './index.css'
 import GenderImage from '../GenderImage'
 
 export default function GenderButton(props) {
-  return (
-    <a
-      className={props.selecionado ? "gender-button selected-gender-button" : "gender-button"}
-      href="#!"
-      onClick={e => props.atualizarGenero(e,props.genero)}
-    >
-      <GenderImage genero={props.genero} />
-    </a>
-
-  )
+    return (
+        <a 
+            className={
+                props.selecionado ?
+                "gender-button selected-gender-button" :
+                "gender-button"
+            }
+            href="#!"
+            onClick={e => props.atualizarGenero(e,props.genero)}
+        >
+            <GenderImage
+                genero={props.genero}
+            />
+        </a>
+    )
 }
